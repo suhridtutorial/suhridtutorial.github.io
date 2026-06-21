8aeaea5aca367cab56199948dfaee87661eb9ec0
suhridtutorial <suhrid2304@gmail.com>
2026-06-21 12:47:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ============ EDIT THESE TWO LINES ============
-window.SUPABASE_URL  = "https://YOUR-PROJECT.supabase.co";
-window.SUPABASE_ANON = "YOUR-ANON-PUBLIC-KEY";
+window.SUPABASE_URL  = "https://dltxlwgobazqtmtidlca.supabase.co";
+window.SUPABASE_ANON = "sb_publishable_xmy8cDRfxepYhK0gY-9t5g_hTjwDrxY";
 // ==============================================
 window.APP_NAME = "Suhrid Tutorial";
 window.AUTH_DOMAIN = "suhrid.local"; // userid -> userid@suhrid.local
